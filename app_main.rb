f97567454f45d412c6d60490d65c1b349eed4bff
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-10-14 17:44:05 +0900

    gcal deploy test

--- app_main.rb
+++ app_main.rb
@@ -1,99 +1,101 @@
-# require 'google/apis/calendar_v3'
-# require 'googleauth'
-# require 'googleauth/stores/file_token_store'
-# require 'fileutils'
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
 
-# OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
-# APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'
-# CLIENT_SECRETS_PATH = 'client_secret.json'
-# CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
-#                              "calendar-ruby-quickstart.yaml")
-# SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'
+CLIENT_SECRETS_PATH = 'client_secret.json'
+CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+                             "calendar-ruby-quickstart.yaml")
+SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
 
-# def authorize
-#   FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+def authorize
+  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
 
-#   client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
-#   token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
-#   authorizer = Google::Auth::UserAuthorizer.new(
-#     client_id, SCOPE, token_store)
-#   user_id = 'default'
-#   credentials = authorizer.get_credentials(user_id)
-#   if credentials.nil?
-#     url = authorizer.get_authorization_url(
-#       base_url: OOB_URI)
-#     puts "Open the following URL in the browser and enter the " +
-#          "resulting code after authorization"
-#     puts url
-#     code = gets
-#     credentials = authorizer.get_and_store_credentials_from_code(
-#       user_id: user_id, code: code, base_url: OOB_URI)
-#   end
-#   credentials
+  client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(
+    client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(
+      base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the " +
+         "resulting code after authorization"
+    puts url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
+  end
+  credentials
+end
+
+# Initialize the API
+service = Google::Apis::CalendarV3::CalendarService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+
+# Fetch the next 10 events for the user
+calendar_id = 'primary'
+response = service.list_events(calendar_id,
+                               max_results: 10,
+                               single_events: true,
+                               order_by: 'startTime',
+                               time_min: Time.now.iso8601)
+
+puts "Upcoming events:"
+puts "No upcoming events found" if response.items.empty?
+response.items.each do |event|
+  start = event.start.date || event.start.date_time
+  puts "- #{event.summary} (#{start})"
+
+end
+
+
+# require 'sinatra'
+# require 'line/bot'
+
+# # 微小変更部分！確認用。
+# get '/' do
+#   "Hello world"
 # end
 
-# # Initialize the API
-# service = Google::Apis::CalendarV3::CalendarService.new
-# service.client_options.application_name = APPLICATION_NAME
-# service.authorization = authorize
+# def client
+#   @client ||= Line::Bot::Client.new { |config|
+#     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+#     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+#   }
+# end
 
-# # Fetch the next 10 events for the user
-# calendar_id = 'primary'
-# response = service.list_events(calendar_id,
-#                                max_results: 10,
-#                                single_events: true,
-#                                order_by: 'startTime',
-#                                time_min: Time.now.iso8601)
+# post '/callback' do
+#   body = request.body.read
 
-# puts "Upcoming events:"
-# puts "No upcoming events found" if response.items.empty?
-# response.items.each do |event|
-#   start = event.start.date || event.start.date_time
-#   puts "- #{event.summary} (#{start})"
+#   signature = request.env['HTTP_X_LINE_SIGNATURE']
+#   unless client.validate_signature(body, signature)
+#     error 400 do 'Bad Request' end
+#   end
 
+#   events = client.parse_events_from(body)
+#   events.each { |event|
+#     case event
+#     when Line::Bot::Event::Message
+#       case event.type
+#       when Line::Bot::Event::MessageType::Text
+#         text="hallo World!"
+#         message = {
+#           type: 'text',
+#           text: "Hello World!"
+#         }
+#         client.reply_message(event['replyToken'], message)
+#       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+#         response = client.get_message_content(event.message['id'])
+#         tf = Tempfile.open("content")
+#         tf.write(response.body)
+#       end
+#     end
+#   }
 
-require 'sinatra'
-require 'line/bot'
-
-# 微小変更部分！確認用。
-get '/' do
-  "Hello world"
-end
-
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
-
-post '/callback' do
-  body = request.body.read
-
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
-
-  events = client.parse_events_from(body)
-  events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        text="hallo World!"
-        message = {
-          type: 'text',
-          text: "Hello World!"
-        }
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
-      end
-    end
-  }
-
-  "OK"
-end
+#   "OK"
+# end
