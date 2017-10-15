f21627dd766899c0ee6918fdfe25443ba572cb2a
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-10-15 13:52:54 +0900

    fix

--- app_main.rb
+++ app_main.rb
@@ -36,9 +36,6 @@ post '/callback' do
   events = client.parse_events_from(body)
   @paraData=[]  #空の配列を作成
   @paraData=File.open("notebook.txt").readlines
-  @paraData each do |data|
-  puts data
-  end
 
   events.each { |event|
     case event
